49d2ce63e9e9b0d24c7b849e2cefd0be1627252d
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-02-20 16:42:10 +0600

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -11,21 +11,31 @@ export default function Home() {
       <div>
         <Banner></Banner>
       </div>
+<<<<<<< HEAD
       <div>
         <Global></Global>
       </div>
+=======
+>>>>>>> 5fa67de (add footer)
       <div className="bg-gray-100">
         <NetworkSolutions></NetworkSolutions>
       </div>
       <div>
+<<<<<<< HEAD
         {/* <LottiFile></LottiFile> */}
       </div>
       <div>
+=======
+>>>>>>> 5fa67de (add footer)
         <Package></Package>
       </div>
       <div>
         <OfferSection></OfferSection>
       </div>
+<<<<<<< HEAD
+=======
+
+>>>>>>> 5fa67de (add footer)
     </div>
   );
 }
