49298a2081ac0928d60aa213298e446b02896bbf
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-03-11 01:20:40 +0600

    remove unused import for LottiFile component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
+
 import Banner from "./components/HomePage/Banner/Banner";
 import Package from "./components/HomePage/Package/Package";
 import NetworkSolutions from "./components/NetworkSolutions/NetworkSolutions ";
 import OfferSection from "./components/OfferSection/OfferSection";
 import Global from "./components/Global/Global";
-import LottiFile from "./components/LottiFile/LottiFile";
 
 export default function Home() {
   return (
